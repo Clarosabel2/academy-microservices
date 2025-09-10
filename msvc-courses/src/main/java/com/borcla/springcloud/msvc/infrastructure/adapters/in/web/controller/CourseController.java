192e1f1df7921bafe737e4aaf27951fa26524c35
Christian Claros <95495732+Clarosabel2@users.noreply.github.com>
2025-09-09 22:15:46 -0300

    Use ResponseEntity.of in findCourseById

--- msvc-courses/src/main/java/com/borcla/springcloud/msvc/infrastructure/adapters/in/web/controller/CourseController.java
+++ msvc-courses/src/main/java/com/borcla/springcloud/msvc/infrastructure/adapters/in/web/controller/CourseController.java
@@ -1,5 +1,6 @@
 package com.borcla.springcloud.msvc.infrastructure.adapters.in.web.controller;
 
+import com.borcla.springcloud.msvc.application.exception.CourseNotFoundException;
 import com.borcla.springcloud.msvc.application.services.CourseService;
 import com.borcla.springcloud.msvc.domain.model.Course;
 import com.borcla.springcloud.msvc.infrastructure.adapters.in.web.dto.request.CreateCourseRequest;
@@ -7,14 +8,20 @@ import com.borcla.springcloud.msvc.infrastructure.adapters.in.web.dto.request.Up
 import com.borcla.springcloud.msvc.infrastructure.adapters.in.web.dto.response.CourseCreatedResponse;
 import com.borcla.springcloud.msvc.infrastructure.adapters.in.web.dto.response.CourseSummaryResponse;
 import com.borcla.springcloud.msvc.infrastructure.adapters.in.web.mapper.ICourseWebMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/api/v1/courses")
 public class CourseController {
+
+    private static final Logger log = LoggerFactory.getLogger(CourseController.class);
 
     private final CourseService courseService;
     private final ICourseWebMapper courseWebMapper;
@@ -26,7 +33,19 @@ public class CourseController {
 
     @GetMapping("/{id}")
     public ResponseEntity<CourseSummaryResponse> findCourseById(@PathVariable Long id){
-        return ResponseEntity.ok(courseWebMapper.toSummaryDto(courseService.getCourseById(id)));
+        log.info("Retrieving course with id {}", id);
+
+        Course course = null;
+        try {
+            course = courseService.getCourseById(id);
+        } catch (CourseNotFoundException e) {
+            log.warn("Course with id {} not found", id);
+        }
+
+        return ResponseEntity.of(
+                Optional.ofNullable(course)
+                        .map(courseWebMapper::toSummaryDto)
+        );
     }
 
     @GetMapping
